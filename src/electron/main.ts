f8cbd28d2bcffe3fdd4895dca8ac17f12c902ece
LsDri92 <luciano.s.dri@gmail.com>
2025-04-03 09:21:44 -0300

    commit

---
+++ src/electron/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+  });
+
+  // Cargar la aplicación de React (en desarrollo apuntar a localhost)
+  // En producción se cargará el build de la app
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:3000');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../../build/index.html'));
+  }
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ src/electron/preload.ts
@@ -0,0 +1,7 @@
+// Puedes exponer de forma segura ciertas APIs al renderer
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel: string, data: any) => ipcRenderer.send(channel, data),
+  receive: (channel: string, func: (...args: any[]) => void) => ipcRenderer.on(channel, (event, ...args) => func(...args))
+});
